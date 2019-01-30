0c5da22e6e38059760eade2264aaeb228c7fd9e6
Nicos Angelopoulos <nicos@stoics.org.uk>
2019-01-30 15:40:07 +0000

    UPDATE:  new way of locating STRING version (valid from STRING 11.0)

--- auxil/lib/std_graphs_strg_auto_version.pl
+++ auxil/lib/std_graphs_strg_auto_version.pl
@@ -16,12 +16,21 @@ Vers = '10.5'.
 */
 std_graphs_strg_auto_version( VerAtm ) :-
     load_html( 'https://string-db.org/', Idx, [] ),
-    findall( Li, xpath(Idx,//(li),Li), Lis ),
-    Lis = [_,element(li,_,[element(a,_,[VerAtm])])|_],
-    % element(li, [class=last], [element(a, _, ['10.5'])])
-    atomic_list_concat( IntAtms, '.', VerAtm ),
-    maplist( atom_number, IntAtms, Ints ),
-    maplist( integer, Ints ),
+    % findall( Li, xpath(Idx,//(li),Li), Lis ),
+    % Lis = [_,element(li,_,[element(a,_,[VerAtm])])|_],
+    % % element(li, [class=last], [element(a, _, ['10.5'])])
+    % atomic_list_concat( IntAtms, '.', VerAtm ),
+    % maplist( atom_number, IntAtms, Ints ),
+    % maplist( integer, Ints ),
+    % As of version 11.0:
+    Idx = [element(_,_,Subs)],
+    Subs = [element(head,[],HeadSubs),_Body], 
+    member( element(script, [], [ScriptSub] ), HeadSubs ), 
+    atom_concat( Pfx, Mdx, ScriptSub ), 
+    atom_concat( _, 'string_database_version_dotted: \'', Pfx ), 
+    atom_concat(Left,_Right,Mdx), 
+    atom_concat( VerAtm, '\'', Left ), 
+    atom_number( VerAtm, _Vers ), 
     debug( std_graphs_strg, 'Auto detected version: ~a', VerAtm ),
     !.
 std_graphs_strg_auto_version( unkonwn ) :-

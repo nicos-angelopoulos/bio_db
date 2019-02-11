bd88b3d507b8bd7da24911ac39e4638cca0bca49
Nicos Angelopoulos <nicos@stoics.org.uk>
2019-02-11 16:43:58 +0000

    IFACE:   map_mgim_mouse_mgim_entz/2 DOC:     1 example per mgi_mouse predicate

--- cell/mouse/mgim.pl
+++ cell/mouse/mgim.pl
@@ -2,6 +2,7 @@
                 bio_db_mouse_mgim/0,
                 %       + MGI database: fixme: url
                 map_mgim_mouse_mgim_chrl/5,
+                map_mgim_mouse_mgim_entz/2,
                 map_mgim_mouse_mgim_genb/2,
                 map_mgim_mouse_mgim_symb/2,
                 map_mgim_mouse_mgim_unip/2,
@@ -38,31 +39,66 @@ MGI marker to chromosomal location.
 == 
 ?- map_mgim_mouse_mgim_symb(Mgim,'Lmtk3'), 
    map_mgim_mouse_mgim_chrl( Mgim, Chr, Start, End, Strand ).
+
+Mgim = 3039582,
+Chr = 7,
+Start = 45783738,
+End = 45804144,
+Strand =  (+).
+
 ==
 
 */
 map_mgim_mouse_mgim_chrl( Mgi, Chr, Sta, End, Sgn ) :-
     bio_db:bio_db_serve( map_mgim_mouse_mgim_chrl(Mgi,Chr,Sta,End,Sgn) ).
 
-/**  map_mgim_mouse_mgim_genb( +UniP, -GenB ).
+/**  map_mgim_mouse_mgim_genb( +Mgim, -GenB ).
 
 Map predicate from MGI marker to Gene Bank ids.
 
 ==
-?-  map_mgim_mouse_mgim_sybm( Mgim, 'Lmtk3' ),
-    map_mgim_mouse_mgim_genb( Mgim, GenB ).
+?- map_mgim_mouse_mgim_symb( Mgim, 'Lmtk3' ), 
+   map_mgim_mouse_mgim_genb( Mgim, GenB ), write( GenB ), nl, fail.
 
+AB288873
+AI413247
+AK029204
+AW046037
+BC057032
+BC059845
+BC083185
+BC094377
+BE981921
+false.
+?-  
 ==
 */
 map_mgim_mouse_mgim_genb( X, Y ) :-
     bio_db:bio_db_serve( map_mgim_mouse_mgim_genb(X,Y) ).
+
+/**  map_mgim_mouse_mgim_entz( +Mgim, -Entz ).
+
+Map predicate from MGI marker to NCBI, Entrez ids.
+
+==
+?-  map_mgim_mouse_mgim_symb( Mgim, 'Lmtk3' ),
+    map_mgim_mouse_mgim_entz( Mgim, Entz ).
+
+Entz = 381983,
+Mgim = 3039582.
+
+==
+*/
+map_mgim_mouse_mgim_entz( X, Y ) :-
+    bio_db:bio_db_serve( map_mgim_mouse_mgim_entz(X,Y) ).
 
 /**  map_mgim_mouse_mgim_symb( +Mgim, -Symb ).
 
 Map predicate between MGI marker and (MGI) symbols.
 
 ==
-?- map_mgim_mouse_mgim_sybm( Mgim, 'Lmtk3' ).
+?- map_mgim_mouse_mgim_symb( Mgim, 'Lmtk3' ).
+Mgim = 3039582.
 ==
 */
 map_mgim_mouse_mgim_symb( X, Y ) :-
@@ -73,9 +109,11 @@ map_mgim_mouse_mgim_symb( X, Y ) :-
 Map predicate from MGI markder ids to Uniprot ids.
 
 ==
-?- map_mgim_mouse_mgim_sybm( Mgim, 'Lmtk3' ),
-   map_mgim_mouse_mgim_unip( MgiM, Unip ).
+?- map_mgim_mouse_mgim_symb( Mgim, 'Lmtk3' ),
+   map_mgim_mouse_mgim_unip( Mgim, Unip ).
 
+Mgim = 3039582,
+Unip = 'Q5XJV6'.
 ==
 */
 map_mgim_mouse_mgim_unip( Sprt, Seqn ) :-
@@ -87,6 +125,9 @@ Map predicate from MGI symbols to withdrawn names.
 
 ==
 ?- map_mgim_mouse_symb_wdra( S, W ).
+S = 15006,
+W = 'H2-Q1' ;
+...
 ==
 */
 map_mgim_mouse_symb_wdra( Symb, Wdra ) :-
@@ -97,7 +138,15 @@ map_mgim_mouse_symb_wdra( Symb, Wdra ) :-
 Map predicate from Synonym to MGI marker ID.
 
 ==
-?- map_mgim_mouse_syno_mgim( Syno, MgiM ).
+?- map_mgim_mouse_mgim_symb( Mgim, 'Lmtk3' ),
+|    map_mgim_mouse_syno_mgim( Syno, Mgim ).
+
+Mgim = 3039582,
+Syno = 'AATYK3' ;
+Mgim = 3039582,
+Syno = 'Aatyk3'.
+==
+
 */
 map_mgim_mouse_syno_mgim( X, Y ) :-
     bio_db:bio_db_serve( map_mgim_mouse_syno_mgim(X,Y) ).

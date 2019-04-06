265836775219556191e2c167cfd7cf1165246d98
Nicos Angelopoulos <nicos.angelopoulos@gmail.com>
2019-04-06 18:40:53 +0100

    IFACE:    map_gont_mouse_gont_symb/2

--- auxil/build_repo/mouse/std_mouse_maps_gont.pl
+++ auxil/build_repo/mouse/std_mouse_maps_gont.pl
@@ -64,9 +64,23 @@ std_mouse_maps_gont( Args ) :-
     portray_clauses( Facts, file(MapF) ),
 
 	bio_db_dnt_times( GzGontF, DnDt, _SwDnEn ),
-	InfoOpts = [header(row('MGI_Marker','Evidence','GO_Term')),source(Url),datetime(DnDt)],
+	InfoOpts = [header(row('MGI Marker Accession ID','Evidence','GO_Term')),source(Url),datetime(DnDt)],
 	bio_db_add_infos_to( InfoOpts, 'maps/map_gont_mouse_mgim_gont.pl' ),
+
+    ensure_loaded( mgim:bio_db_build_downloads('mgim/maps/map_mgim_mouse_mgim_symb') ),
+    
+    findall( row(Gont1,Symb1), ( member(map_gont_mouse_mgim_gont(Mgim1,_,Gont1),Facts),
+                                 map_mgim_mouse_mgim_symb(Mgim1,Symb1)
+                               ), GSRowsAll ),
+    sort( GSRowsAll, GSRows ),
+
+    GSopts = [predicate_name(map_gont_mouse_gont_symb)],
+	mtx_prolog( GSRows, 'maps/map_gont_mouse_gont_symb.pl', GSopts ),
+    GsF = 'maps/map_gont_mouse_gont_symb.pl',
+    GShdr = header(row('GO_Term','MGI Marker Accession ID')),
+	bio_db_add_infos_to( [GShdr|GSopts], GsF ),
+    % maplist( link_to_map_sub(gont), OutFs ),  % does this work ?
+    link_to_bio_sub( mouse, gont, maps, GsF  ),
     link_to_bio_sub( mouse, gont, maps, MapF ),
     @ rm( -f, GontF ),
-    % here make link to ../?/data/
     working_directory( _, Old ).

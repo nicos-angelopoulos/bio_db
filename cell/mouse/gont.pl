b9910526e05a59a534be8d15736b1379c54165b5
Nicos Angelopoulos <nicos.angelopoulos@gmail.com>
2019-04-06 21:40:52 +0100

    BUILD:    map_gont_mouse_gont_symb/2 IFACE:    map_gont_mouse_gont_symb/2

--- cell/mouse/gont.pl
+++ cell/mouse/gont.pl
@@ -1,7 +1,8 @@
 :- module( bio_db_mouse_gont, [
                 bio_db_mouse_gont/0,
                 %       + Gene ontology
-                map_gont_mouse_mgim_gont/3
+                map_gont_mouse_mgim_gont/3,
+                map_gont_mouse_gont_symb/2
                 ] ).
 
 :- use_module( library(lib) ).
@@ -34,3 +35,31 @@ MGI marker to gene ontology term and evidence.
 */
 map_gont_mouse_mgim_gont( Mgi, Evid, Gont ) :-
     bio_db:bio_db_serve( map_gont_mouse_mgim_gont(Mgi,Evid,Gont) ).
+
+/** map_gont_mouse_gont_symb( +GoTerm, -Symb ).
+
+GO term to mouse Symbol.
+
+== 
+?- map_gont_mouse_gont_symb(Mgim,'Lmtk3').
+Go = 166 ;
+Go = 4672 ;
+Go = 4674 ;
+Go = 5524 ;
+Go = 5794 ;
+Go = 6468 ;
+Go = 10923 ;
+Go = 16020 ;
+Go = 16021 ;
+Go = 16301 ;
+Go = 16310 ;
+Go = 16740 ;
+Go = 42995 ;
+Go = 46872.
+==
+
+@version 0:1 2019/4/6
+
+*/
+map_gont_mouse_gont_symb( Gont, Symb ) :-
+    bio_db:bio_db_serve( map_gont_mouse_gont_symb(Gont,Symb) ).

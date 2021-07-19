4fa4003f0b1de44a4c2db02832a5ec41c3fef42b
Nicos Angelopoulos <nicos@gmx.co.uk>
2021-07-19 13:25:32 +0100

    CHANGED:    update test_repo.pl

--- auxil/test_repo/test_repo.pl
+++ auxil/test_repo/test_repo.pl
@@ -1,30 +1,39 @@
 
 :- ensure_loaded(library(lib)).
 
-:- lib(bio_db).
 :- lib(debug_call).
+
+:- lib(bio_db).
+:- lib(bio_db_repo).
 
 :- debuc(test_repo).
 
 /** test_repo.
 
-    Test the current candidate for next bio_db_repo- from its downladed area, without installing proper first.
+    Test the current candidate for next pack(bio_db_repo). 
+
+    Assumes it has been installed locally.
+    Also make sure you installed the latest bio_db. The predicate tries to call each predicate imported from
+    modules starting with _bio_db_hs_ or _bio_db_mouse_ and is prefixed by _map_ or _edge_.
 
 @author nicos angelopoulos
 @version  0.1 2017/10/14
 
 */
 test_repo :-
-    debug_call( test_repo, start, true ),
+    debuc( test_repo, start, true ),
+    bio_db_repo_version( Version, Date ),
+    debuc( test_repo, 'Version: ~w, published on: ~w', [Version,Date] ),
     test_repo_preds,
-    debug_call( test_repo, end, true ).
+    debuc( test_repo, end, true ).
 
 test_repo_preds :-
-    predicate_property( Head, imported_from(bio_db) ),
+    predicate_property( Head, imported_from(From) ),
+    ( atom_concat(bio_db_hs,_,From) ; atom_concat(bio_db_mouse,_,From) ),
     functor( Head, Pname, _Arity ),
     ( atom_concat(map,_,Pname) ; atom_concat(edge,_,Pname) ),
-    debug( test_repo, 'Head: ~w', [Head] ),
+    debuc( test_repo, 'Head: ~w', [Head] ),
     once( Head ),
-    debug( test_repo, 'First: ~w', [Head] ),
+    debuc( test_repo, 'First: ~w', [Head] ),
     fail.
 test_repo_preds.

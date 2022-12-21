daeded1102602d0567d001639445b376c620754a
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-21 09:03:31 +0000

    CELL:  populate gg6a ensembl tables

--- cell/gallus/ense.pl
+++ cell/gallus/ense.pl
@@ -3,7 +3,11 @@
                 map_ense_gallus_enst_chrl/5,
                 map_ense_gallus_ensg_chrl/5,
                 map_ense_gallus_enst_ensg/2,
-                map_ense_gallus_ensg_symb/2
+                map_ense_gallus_ensg_symb/2,
+                map_ense_gg6a_enst_chrl/5,
+                map_ense_gg6a_ensg_chrl/5,
+                map_ense_gg6a_enst_ensg/2,
+                map_ense_gg6a_ensg_symb/2
              ]
          ).
 
@@ -68,3 +72,54 @@ Ensembl Transcript to Ensembl Gene id with data drawn from Ensembl.
 */
 map_ense_gallus_enst_ensg( EnsT, EnsG ) :-
     bio_db:bio_db_serve( map_ense_gallus_enst_ensg(EnsT,EnsG) ).
+
+/**  map_ense_gg6a_ensg_symb( ?EnsG, ?MgimID ).
+
+Ensembl gene id to symbol with data drawn from Ensembl.
+
+==
+?- map_ense_gg6a_ensg_symb( A, B ).
+==
+*/
+map_ense_gg6a_ensg_symb( EnsG, Symb ) :-
+    bio_db:bio_db_serve( map_ense_gg6a_ensg_symb(EnsG,Symb) ).
+
+/**  map_ense_gg6a_enst_chrl( +EnsT, -Chr, -Start, -End, -Dir ).
+
+Ensembl transcript chromosomal location.
+
+Chr is the chromosome, Start the start position, End the end position and 
+Dir is the direction of the transcript.
+
+==
+?- map_ense_gg6a_enst_chrl( EnsT, Chr, Start, End, Dir ).
+==
+
+*/
+map_ense_gg6a_enst_chrl( EnsT, Chr, Start, End, Dir ) :-
+    bio_db:bio_db_serve( map_ense_gg6a_enst_chrl(EnsT,Chr,Start,End,Dir) ).
+
+
+/*  map_ense_gg6a_ensg_chrl( +EnsG, -Chr, -Start, -End, -Dir ).
+
+Ensembl gene id to chromosomal location.
+Chr is the chromosome, Start the start position, End the end position and 
+Dir is the direction of the transcript.
+
+==
+?- map_ense_gg6a_ensg_chrl( EnsG, Chr, Start, End, Dir ).
+==
+*/
+map_ense_gg6a_ensg_chrl( EnsG, Chr, Start, End, Dir ) :-
+    bio_db:bio_db_serve( map_ense_gg6a_ensg_chrl(EnsG,Chr,Start,End,Dir) ).
+
+/**  map_ense_gg6a_enst_ensg( +EnsT, -EnsG ).
+
+Ensembl Transcript to Ensembl Gene id with data drawn from Ensembl.
+
+==
+?- map_ense_mouse_enst_ensg( EnsT, EnsG ).
+==
+*/
+map_ense_gg6a_enst_ensg( EnsT, EnsG ) :-
+    bio_db:bio_db_serve( map_ense_gg6a_enst_ensg(EnsT,EnsG) ).

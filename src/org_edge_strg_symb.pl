8ffe141accdfe40eaaa676e8da2548c85fa31c33
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-21 17:37:32 +0000

    ORG:  gallus (gallus gallus) support in org_edge_strg_symb/4

--- src/org_edge_strg_symb.pl
+++ src/org_edge_strg_symb.pl
@@ -1,6 +1,8 @@
-/** org_edge_strg_symb( +Org, -Symb1, Symb2, W )
+/** org_edge_strg_symb( +Org, +Symb1, +Symb2, -W )
 
-Organism abstraction interface to STRING database access.
+Organism abstraction interface to STRING database test.
+
+Assumes underlying predicates have their symbols sorted, and that Org is already passed through bio_organism/2.
 
 ==
 ?- org_edge_strg_symb( hs, 'LMTK3', Symb2, W ).
@@ -14,14 +16,17 @@ W = 308.
 
 @author nicos angelopoulos
 @version  0:1 2019/4/8
+@version  0:2 2022/12/8
 
 */
 org_edge_strg_symb( Org, Symb1, Symb2, W ) :-
     sort( [Symb1,Symb2], [SymbA,SymbB] ),
     org_edge_strg_symb_ord( Org, SymbA, SymbB, W ).
 
+org_edge_strg_symb_ord( gallus, SymbA, SymbB, W ) :-
+    edge_strg_gallus_symb( SymbA, SymbB, W ).
 org_edge_strg_symb_ord( human, SymbA, SymbB, W ) :-
-    org_edge_strg_symb_ord( hs, SymbA, SymbB, W ).
+	edge_strg_hs_symb(SymbA,SymbB,W).
 org_edge_strg_symb_ord( hs, SymbA, SymbB, W ) :-
 	edge_strg_hs_symb(SymbA,SymbB,W).
 org_edge_strg_symb_ord( mouse, SymbA, SymbB, W ) :-

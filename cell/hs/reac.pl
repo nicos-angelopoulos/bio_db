1b2d3a4305ee2daa4af26a21d844b848c451dac9
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-25 19:10:12 +0000

    CELL:    hs/reac.pl

---
+++ cell/hs/reac.pl
@@ -0,0 +1,101 @@
+:- module( bio_db_homs_strg, [
+                    bio_db_homs_reac/0,
+                    %       + Reactome
+                    reac_homs_ncbi_reac/2,
+                    reac_homs_ncbi_reap/2,
+                    reac_homs_reac_reap/3,
+                    reac_homs_reac_recl/2,
+                    reac_homs_reac_recn/2,
+                    reac_homs_reap_repn/2
+                ] ).
+
+:- use_module(library(lib)).
+:- lib(bio_db).
+
+/**  bio_db_homs_strg.
+
+Documentation predicate for Homo sapiens data from reactome database.
+
+==
+?- lib( &bio_db(homs(strg)) ).
+?- [ pack('bio_db/cell/homs/strg') ].
+==
+
+@author nicos angelopoulos
+@version  0.1 2018/10/29
+
+*/
+bio_db_homs_reac.
+
+/**  reac_homs_ncbi_reac( ?Ncbi, ?Reac ).
+
+Map (human) NCBI identifiers to Reactome product ids.
+
+==
+?- reac_homs_ncbi_reac( 1000, Reac ).
+Reac = 191429 ;
+Reac = 8956761 ;
+Reac = 8956899.
+==
+
+
+*/
+reac_homs_ncbi_reac( X, Y, Z ) :-
+    bio_db:bio_db_serve( reac_homs_ncbi_reac(X,Y,Z) ).
+
+/**  reac_homs_ncbi_reap( ?Ncbi, -Evidence, ?Reap ).
+
+Map (human) NCBI identifiers to Reactome pathway ids.
+
+Evidence is the type of evidence supporting the pathway membership.
+
+==
+?- reac_homs_ncbi_reap(1000, Evi, Reac), write(Evi-Reac), nl.
+IEA-525793
+IEA-1266738
+TAS-381426
+TAS-392499
+TAS-418990
+...
+==
+
+*/
+reac_homs_ncbi_reap( X, Y, Z ) :-
+    bio_db:bio_db_serve( reac_homs_ncbi_reap(X,Y,Z) ).
+
+/**  reac_homs_reac_recl( ?Reac, ?Recl ).
+
+Map Reactome product identifiers to localisation tokens.
+
+==
+?- reac_homs_reac_recl( 191429, Recl ).
+Recl = 'plasma membrane'.
+==
+*/
+reac_homs_reac_recl( X, Y, Z ) :-
+    bio_db:bio_db_serve( reac_homs_reac_recl(X,Y,Z) ).
+
+/**  reac_homs_reac_recn( ?Reac, ?Recn ).
+
+Map Reactome product identifiers to product names.
+
+==
+?- reac_homs_reac_recn( 191429, Recn ).
+Recn = 'CDH2'.
+==
+*/
+reac_homs_reac_recn( X, Y, Z ) :-
+    bio_db:bio_db_serve( reac_homs_reac_recn(X,Y,Z) ).
+
+/**  reac_homs_reap_repn( ?Reap, ?Repn ).
+
+Map Reactome pathway identifiers to pathway names.
+
+==
+?- reac_homs_reap_repn( 525793, Repn ).
+Repn = 'Myogenesis'.
+==
+*/
+reac_homs_reap_repn( X, Z ) :-
+    bio_db:bio_db_serve( reac_homs_reap_repn(X,Z) ).
+

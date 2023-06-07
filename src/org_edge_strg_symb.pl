93043bbacbda2933be809df1a2e7afd81f35dc9d
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-06-07 23:04:39 +0100

    UPDATED:  table names to new regime, + support for pig

--- src/org_edge_strg_symb.pl
+++ src/org_edge_strg_symb.pl
@@ -17,6 +17,7 @@ W = 308.
 @author nicos angelopoulos
 @version  0:1 2019/4/8
 @version  0:2 2022/12/8
+@version  0:3 2023/6/7,  updated table names to new regime, + support for pig
 
 */
 org_edge_strg_symb( Org, Symb1, Symb2, W ) :-
@@ -24,10 +25,16 @@ org_edge_strg_symb( Org, Symb1, Symb2, W ) :-
     org_edge_strg_symb_ord( Org, SymbA, SymbB, W ).
 
 org_edge_strg_symb_ord( gallus, SymbA, SymbB, W ) :-
-    edge_strg_gallus_symb( SymbA, SymbB, W ).
+    % edge_strg_gallus_symb( SymbA, SymbB, W ).
+    strg_galg_edge_symb( SymbA, SymbB, W ).
 org_edge_strg_symb_ord( human, SymbA, SymbB, W ) :-
-	edge_strg_hs_symb(SymbA,SymbB,W).
+	% edge_strg_hs_symb(SymbA,SymbB,W).
+     strg_homs_edge_symb( SymbA, SymbB, W ).
 org_edge_strg_symb_ord( hs, SymbA, SymbB, W ) :-
-	edge_strg_hs_symb(SymbA,SymbB,W).
+	% edge_strg_hs_symb(SymbA,SymbB,W).
+     strg_homs_edge_symb( SymbA, SymbB, W ).
 org_edge_strg_symb_ord( mouse, SymbA, SymbB, W ) :-
-	edge_strg_mouse_symb(SymbA,SymbB,W).
+	% edge_strg_mouse_symb(SymbA,SymbB,W).
+     strg_musm_edge_symb( SymbA, SymbB, W ).
+org_edge_strg_symb_ord( pig, SymbA, SymbB, W ) :-
+     strg_suss_edge_symb( SymbA, SymbB, W ).

3c0beff85fa9fed375fc5084e7700fde85289633
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-23 17:11:45 +0100

    FIXED:  adjust to recent changes in map_predicate_name/4

--- auxil/build_repo/lib/map_uniprot.pl
+++ auxil/build_repo/lib/map_uniprot.pl
@@ -6,11 +6,12 @@
 :- lib(debug_call).
 :- lib( stoics_lib:en_list/2 ).
 
-map_uniprot_defaults( [   uniprot('HUMAN_9606_idmapping.dat'),
-                          interface(prolog),
-					 f_call(=),
-					 ext([]),
+map_uniprot_defaults( [   db(unip),
 					 destination(maps),
+					 ext([]),
+					 f_call(=),
+                          id_map('HUMAN_9606_idmapping.dat'),
+                          interface(prolog),
 					 reverse(false)
 					    ] ).
 
@@ -32,7 +33,7 @@ calls to map_uniprot/3 to utilise the read Csv.
 Written is the list of all files written on.
 
 Options
-  * uniprot('HUMAN_9606_idmapping.dat.gz')
+  * id_map('HUMAN_9606_idmapping.dat.gz')
     file- only used if UniprotCsv is an unbound variable
   * f_call(=)
     call to transform input Foreign accession to storable accession
@@ -55,6 +56,7 @@ Options
 
 @author nicos angelopoulos
 @version  0.1 2014/7/2
+@see map_predicate_name/4
 */
 map_uniprot( Foreign ) :- 
 	map_uniprot( Foreign, _, _, [] ).
@@ -62,7 +64,7 @@ map_uniprot( Foreign, Opts ) :-
 	map_uniprot( Foreign, _, _, Opts ).
 map_uniprot( Foreign, Uniprot, Fouts, Args ) :-
 	options_append( map_uniprot, Args, Opts ),
-	memberchk( uniprot(UniprotFile), Opts ),
+	memberchk( id_map(UniprotFile), Opts ),
 	memberchk( f_call(Fcall), Opts ),
 	uniprot_load( UniprotFile, Uniprot ),
 	findall( row(Prot,FAcc),  (
@@ -76,10 +78,8 @@ map_uniprot( Foreign, Uniprot, Fouts, Args ) :-
 	memberchk( destination(Dst), Opts ),
 	memberchk( reverse(Rev), Opts ),
 	reverse_column_names( Rev, uniprot, Foreign, Cnm1, Cnm2 ),
-
-	% map_predicate_name( uniprot, Foreign, Pname, Opts ),
 	maplist( uniprot_cname, [Cnm1,Cnm2], [Tnm1,Tnm2] ),
-	map_predicate_name( Tnm1, Tnm2, Pname, [map_prefix(false),prefix(unip)|Opts] ),
+	map_predicate_name( Tnm1, Tnm2, Pname, Opts ),
 	map_predicate_name_stem( Pname, Stem, Opts ),
 	output_extensions( Opts, Fces, FExtsPrv ),
 	sort( FExtsPrv, FExts ),
@@ -98,7 +98,6 @@ uniprot_load( Partial, Rows ) :-
 	!,
 	uniprot_load_file( File, Rows ).
 uniprot_load( Partial, Rows ) :-
-	% expand_file_name( '$local/../work/db/data/uniprot', [UnipD] ),
 	absolute_file_name( bio_db_downloads(unip), UnipD ),
 	working_directory( Old, UnipD ),
 	Old \== UnipD,
@@ -111,7 +110,6 @@ uniprot_load( _Partial, _Rows ) :-
 uniprot_load_file( File, Rows ) :-
 	file_name_extension( Stem, gz, File ),
 	!,
-	% gunzip( File, Stem ),
 	@ gunzip( --keep, --force, File ),
 	\+ file_name_extension( _, gz, Stem ), % just in case
 	uniprot_load_file( Stem, Rows ),
@@ -138,26 +136,11 @@ map_uniprot_save( prolog, Ext, Foreign, Pname, Stem, Dst, PFRows, Rev, File ) :-
 	os_dir_stem_ext( Dst, Stem, Ext, File ),
 	os_dir_stem_ext( Dst, Stem, '', DstStem ),
 	debuc( uniprot, 'Opened prolog output to: ~p', File ),
-	% ComClause =.. [Pname,uniprot_accession,Foreign],
-	% write( Out, '% ' ), portray_clause( Out, ComClause ),
 	findall( U, member(row(U,_),PFRows), Us ),
 	findall( N, member(row(_,N),PFRows), Ns ),
 	IfcOpts = [to_value_1(=),to_value_2(=)],
 	reverse_columns( Rev, 'Uniprot', Foreign, Us, Ns, Cnm1, Cnm2, Clm1, Clm2 ),
-	csv_ids_interface_map( prolog, Pname, DstStem, Cnm1, Cnm2, Clm1, Clm2, _PlFile, IfcOpts ),
-	% csv_ids_interface_map( prolog, Pname, DstStem, 'Uniprot', Foreign, Us, Ns, _PlFile, IfcOpts ),
-	/* why is this :  here? csv_ids_interface_map already writes the damn file???
-	length( PFRows, PFLen ),
-	Itv is floor( PFLen // 10 ),
-	debuc( uniprot, time(start), 'write on file' ),
-	open( File, write, Out ),
-	findall( _, (   nth1(N,PFRows,row(UniP,Fid)),
-				 uniprot_report_progress(N,Itv),
-	                Clause=..[Pname,UniP,Fid],portray_clause(Out,Clause)), _ ),
-	debuc( uniprot, time(finish), 'write on file' ),
-	close( Out ).
-	*/
-	true.
+	csv_ids_interface_map( prolog, Pname, DstStem, Cnm1, Cnm2, Clm1, Clm2, _PlFile, IfcOpts ).
 
 reverse_column_names( false, Cnm1, Cnm2, Cnm1, Cnm2 ).
 reverse_column_names( true, Cnm1, Cnm2, Cnm2, Cnm1 ).
@@ -167,12 +150,6 @@ reverse_columns(  true, Cnm2, Cnm1, Clm2, Clm1, Cnm1, Cnm2, Clm1, Clm2 ).
 
 map_uniprot_save( pl, Ext, Foreign, Pname, Stem, Dst, PFRows, File ) :-
 	map_uniprot_save( prolog, Ext, Foreign, Pname, Stem, Dst, PFRows, File ).
-
-uniprot_report_progress( N, Itv ) :-
-	N mod Itv =:= 0,
-	!,
-	write( '.' ), flush_output.
-uniprot_report_progress( _N, _Itv ).
 
 output_extensions( Opts, Fces, Fexts ) :-
 	memberchk( ext(ExtPrv), Opts ),

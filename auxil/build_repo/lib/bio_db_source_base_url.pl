9ddbcb885d51e47b6ac3016f600bfcf722a92a52
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-25 09:51:34 +0100

    FIXED:  missing GENE_INFO in ncbi base URL

--- auxil/build_repo/lib/bio_db_source_base_url.pl
+++ auxil/build_repo/lib/bio_db_source_base_url.pl
@@ -11,7 +11,7 @@ bio_db_source_base_url(ense_musm, 'ftp://ftp.ensembl.org/pub/current_gtf/mus_mus
 bio_db_source_base_url(gont_goa, 'https://geneontology.org/gene-associations/').
 bio_db_source_base_url(gont_obo, 'https://purl.obolibrary.org/obo/').
 bio_db_source_base_url(hgnc, 'https://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/').
-bio_db_source_base_url(ncbi, 'https://ftp.ncbi.nih.gov/gene/DATA/').
+bio_db_source_base_url(ncbi, 'https://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/').
 bio_db_source_base_url(mgim, 'http://www.informatics.jax.org/downloads/reports/').
 bio_db_source_base_url(pros, 'https://ftp.expasy.org/databases/prosite/').
 bio_db_source_base_url(reac, 'https://reactome.org/download/current/' ).
@@ -19,3 +19,4 @@ bio_db_source_base_url(strg, 'https://stringdb-downloads.org/download/').
 bio_db_source_base_url(unip, 'https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/').
 bio_db_source_base_url(unip_taxo, 'https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/').
 bio_db_source_base_url(useqs, 'https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/').
+% ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO

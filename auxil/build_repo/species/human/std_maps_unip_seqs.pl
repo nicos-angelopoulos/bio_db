4ed0888b1eebe18d0066c4c1acfa8b5f8a53803c
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-25 15:15:03 +0100

    FIXED:  Rename list of Trembl was also mistaken a - for a comma

--- auxil/build_repo/species/human/std_maps_unip_seqs.pl
+++ auxil/build_repo/species/human/std_maps_unip_seqs.pl
@@ -96,7 +96,7 @@ std_maps_unip_seqs( Args ) :-
      bio_db_source_url( SprotUrl, SrcRnms, Opts ),
      working_directory( Old, DnDir ),
 	unip_hs_seqs_file( SprotUrl, DnDir, row('Swiss Prot ID','Sequence'), Opts ),
-     TrmRnms = [useqs_base-url_base,useqs_file_trembl-url_file-debug_url-debug],
+     TrmRnms = [useqs_base-url_base,useqs_file_trembl-url_file,debug_url-debug],
      bio_db_source_url( TremblUrl, TrmRnms, Opts ),
 	unip_hs_seqs_file( TremblUrl, DnDir, row('TrEMBL Prot ID','Sequence'), Opts ),
      % ( catch(pack_remove(bio_db_repo),_,fail) -> true; true ),

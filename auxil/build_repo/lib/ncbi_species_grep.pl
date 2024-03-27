42849a2eebb837e9000c35f47b17164e7f09e777
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-03-27 15:01:51 +0000

    CHANGED:  utilise the new lib preds

--- auxil/build_repo/lib/ncbi_species_grep.pl
+++ auxil/build_repo/lib/ncbi_species_grep.pl
@@ -1,43 +1,44 @@
 
 :- use_module(library(lib)).
 
+% stoics
+:- lib(os_lib).
 :- lib(options).
 :- lib(debug_call).
 
-ncbi_species_grep_defaults([hde(_)]).
+% bio_db/auxil
+:- lib(bio_db_taxo/3).
+:- lib(os_grep_mtx/4).
 
-/** ncbi_species_grep(+File, +Opts).
+ncbi_species_grep_defaults([]).
 
-Greps the species information from an NCBI downloaded (tsv) file.
+/** ncbi_species_grep(+File, -OutF, +Opts).
+
+Greps the species information from an NCBI downloaded (tsv) file to an output file (OutF).
 
 The species information is on the first column by numeric taxonomy. 
+The output file is produced by postfixing File by Org as returned by 1st argument of bio_db_taxo/3.
 
 The predicate prepends a custom hdr if one is given as on option, or replicates the input header 
 (and returnes it in that option term).
 
-Opts
-  * debug(Dbg=false)
-    informational, progress messages
-  * hdr(Hdr) 
-    if given is taken to be in row format and added as 1st line to output (via mtx/2).
-    if not given, the 1st line of the input line is returned
-  * org(
-
+Opts are passed to bio_db_taxo/3 and os_grep_mtx/4.
 
 Examples
 ==
-?- ncbi_species_grep([]).
+?- ncbi_species_grep(a,b,c).
 ==
 
 @author nicos angelopoulos
 @version  0.1 2024/03/27
+@see bio_db_taxo/3
 @see os_grep_mtx/4
 
 */
-ncbi_species_grep( TsvF, Args ) :-
+ncbi_species_grep( TsvF, GreF, Args ) :-
      Self = ncbi_species_grep,
      options_append( Self, Args, Opts ),
-     options( hdr(Hdr), Opts ),
-     ( var(Hdr) -> 
-     grep( Stem, '^9606', HsStem ),
-     debuc( Self, end, true ).
+     bio_db_taxo( Org, Tax, Opts ),
+     atomic_concat( '^', Tax, Patt ),
+     os_postfix( Org, TsvF, GreF )
+     os_grep_mtx( TsvF, Patt, GreF, Opts ).

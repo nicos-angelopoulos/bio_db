c1e15a0132242bded2c3a1225402a3a74cd7f04b
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-03-27 18:47:51 +0000

    CHANGED:  tidy up the file structure, in prep for moving bits to a common lib file

--- auxil/build_repo/species/human/std_maps_ncbi.pl
+++ auxil/build_repo/species/human/std_maps_ncbi.pl
@@ -32,130 +32,11 @@
 :- lib(ens_fa_peptide_gene_rows/2).  % /2, fixme: should be more local
 :- lib(url_file_local_date_mirror/3).
 
-/*
-ncbi_dnload( Loc ) :-
-     absolute_file_name( bio_db_build_downloads(ncbi), Loc ),
-     os_make_path( Loc, debug(true) ).
-     */
-
-% maps_ncbi_ensp_ensg.
-%
-% This is a later addition.
-% See ens_fa_peptide_gene_rows..pl
-%
-maps_ncbi_ensp_ensg :-
-     % Dir = '/usr/local/users/nicos/work/db/data/ncbi',
-     fixme,
-     ncbi_dnload( Dir ),
-     EnsF= 'Homo_sapiens.GRCh38.pep.all.fa',
-     working_directory( Old, Dir ),
-     ens_fa_peptide_gene_rows( EnsF, EnsRows ),
-     csv_ids_map( _CsvF, ensp, ensg, EnsRows, OutF, [prefix(ncbi),header(row('Ensembl Protein','Ensembl Gene'))] ),
-     link_to_bio_sub(ncbi, OutF ),
-     working_directory( _, Old ).
-
-% fixme: this is not in the loop ? either fix or remove
-maps_ncbi_ncbi_gont( Opts ) :-
-     % Dir = '/usr/local/users/nicos/work/db/data/ncbi',
-     ncbi_dnload( Dir ),
-     ncbi_repo( Repo ),
-     os_path( Repo, 'gene2go.gz', Url ),
-     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)] ),
-     working_directory( Old, Dir ),
-     @ rm( -f, gene2go_hs ),
-     @ rm( -f, gene2go ),
-     @ gunzip( -f, -k, 'gene2go.gz' ),
-     % debuc( by_unix ),
-     ncbi_species_grep( gene2go, _HsStem, Opts ),  % pass sep(tab)
-     % os_grep_mtx(gene2go, '^9606', gene2go_hs, true ),
-     % system( 'grep "^9606" gene2go | cat gene2go_hs' ),
-     working_directory( _, Old ).
-
-maps_ncbi_rnuc_symb( Self, DnDir, Opts ) :-
-     debuc( by_unix ),
-     bio_db_source_base_url( ncbi, NcbiRepo ),
-     ncbi_humanise_data( gene2accession, DnDir, NcbiRepo, Old, HsStem, HsUrl, HsDnDt, Opts ),
-     CIMOpts = [ db(ncbi),
-                 to_value_1(de_versionise), to_value_2(is_a_symbol),
-                 datetime(HsDnDt), source(HsUrl), 
-                 header(row('RNA Nucleotide','HGNC Symbol'))
-                 | Opts
-     ],
-     RNAnucl = 'RNA_nucleotide_accession.version',
-     debuc( Self, 'Csv Map for: ~w vs ~w', [RNAnucl,'Symbol'] ),
-     csv_ids_map( HsStem, RNAnucl, 'Symbol', _Csv1, OutF, CIMOpts ),
-
-
-     DNAOpts = [ db(ncbi),
-                 to_value_1(de_versionise), to_value_2(is_a_symbol),
-                 datetime(HsDnDt), source(HsUrl), 
-                 header(row('DNA Nucleotide','HGNC Symbol')) 
-                 | Opts
-     ],
-     GENnucl = 'genomic_nucleotide_accession.version', 
-     debuc( Self, 'Csv Map for: ~w vs ~w', [GENnucl,'Symbol'] ),
-     csv_ids_map( HsStem, GENnucl, 'Symbol', _Csv2, DNAF, DNAOpts ),
-     NcbiSymbOpts = [ db(ncbi),
-                 to_value_1(pos_integer), to_value_2(is_a_symbol),
-                 datetime(HsDnDt), source(HsUrl), 
-                 header(row(ncbi,symbol)) 
-                 | Opts
-     ],
-     csv_ids_map( HsStem, 'GeneID', 'Symbol', _Csv3, NcbiSymbF, NcbiSymbOpts ),
-     % delete_file( TmpF ),
-     os_make_path( maps ),
-     @ mv( -f, OutF, maps ),
-     @ mv( -f, DNAF, maps ),
-     @ mv( -f, NcbiSymbF, maps ),
-     working_directory( _, maps ),
-     link_to_bio_sub(ncbi, OutF ),
-     link_to_bio_sub(ncbi, DNAF ),
-     link_to_bio_sub(ncbi, NcbiSymbF ),
-     working_directory( _, Old ).
-
-/** %unigene is no longer maintained as of Feb.2019
-maps_ncbi_unig_ncbi :-
-     ncbi_dnload( Dir ),
-     ncbi_repo( Repo ),
-     os_path( Repo, 'gene2unigene', Url ),
-     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)] ),
-     working_directory( Old, Dir ),
-     bio_db_dnt_times( 'gene2unigene', UgDnDt, _DnEn ),
-
-     csv_read_file( gene2unigene, [_|Csv], [separator(0'\t),match_arity(false)] ),
-     Hdr = row(ncbi,unig),
-     MOpts = [prefix(ncbi),to_value_1(hs_unig),datetime(UgDnDt),source(Url),header(row('Uni Gene','Entrez ID'))], 
-     csv_ids_map( _, 'unig', 'ncbi', [Hdr|Csv], OutF, MOpts ),
-     os_make_path( maps ),
-     @ mv( -f, OutF, maps ),
-     working_directory( _, maps ),
-     link_to_bio_sub( ncbi, OutF ), 
-     working_directory( _, Old ).
-*/
-
-ncbi_humanise_data( Stem, Dir, Repo, Old, HsStem, Url, DnDt, Opts ) :-
-     file_name_extension( Stem, gz, GzF ),
-     os_path( Repo, GzF, Url ),
-     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)|Opts] ),
-     os_path( Dir, GzF, DnlF ),
-     bio_db_dnt_times( DnlF, DnDt, _DnEn ),
-     working_directory( Old, Dir ),
-     % atomic_list_concat( [Stem,hs], '_', HsStem ),
-     % @ rm( -f, HsStem ),
-     @ rm( -f, Stem ),
-     @ gunzip( -f, -k, GzF ),
-     ncbi_species_grep( Stem, HsStem, Opts ),
-     @ rm( -f, Stem ).
-
 hs_unig( In, In ) :-
      atom_concat( 'Hs.', _, In ).
 
 de_versionise( ProductVersion, Product ) :-
      atomic_list_concat( [Product,_Version], '.', ProductVersion ),
-     !.
-
-is_a_symbol( Symb, Symb ) :-
-     hgnc:hgnc_homs_symb_hgnc( Symb, _ ),
      !.
 
 std_maps_ncbi_defaults( Defs ) :-
@@ -189,7 +70,7 @@ Opts
   * org(Org=human)
     organism
   * sep(tab)
-    the download files are in tsv format
+    the downloaded files are in tsv format
 
 ==
 ?- std_maps_ncbi([]).
@@ -245,6 +126,93 @@ std_maps_ncbi_1( Self, File, Url, DnDt, Opts ) :-
      % csv_ids_map( File, ncbi, 'Symbol', Mtx, NcbiSymbF, [header(row(ncbi,symbol))|Nens] ),
      maplist( link_to_bio_sub(ncbi), [GEnsGF,EnsGGF,GEnsPF,EnsPGF] ).
 
+maps_ncbi_rnuc_symb( Self, DnDir, Opts ) :-
+     debuc( by_unix ),
+     bio_db_source_base_url( ncbi, NcbiRepo ),
+     ncbi_humanise_data( gene2accession, DnDir, NcbiRepo, Old, HsStem, HsUrl, HsDnDt, Opts ),
+     CIMOpts = [ db(ncbi),
+                 to_value_1(de_versionise), to_value_2(is_a_symbol),
+                 datetime(HsDnDt), source(HsUrl), 
+                 header(row('RNA Nucleotide','HGNC Symbol'))
+                 | Opts
+     ],
+     RNAnucl = 'RNA_nucleotide_accession.version',
+     debuc( Self, 'Csv Map for: ~w vs ~w', [RNAnucl,'Symbol'] ),
+     csv_ids_map( HsStem, RNAnucl, 'Symbol', _Csv1, OutF, CIMOpts ),
+     DNAOpts = [ db(ncbi),
+                 to_value_1(de_versionise), to_value_2(is_a_symbol),
+                 datetime(HsDnDt), source(HsUrl), 
+                 header(row('DNA Nucleotide','HGNC Symbol')) 
+                 | Opts
+     ],
+     GENnucl = 'genomic_nucleotide_accession.version', 
+     debuc( Self, 'Csv Map for: ~w vs ~w', [GENnucl,'Symbol'] ),
+     csv_ids_map( HsStem, GENnucl, 'Symbol', _Csv2, DNAF, DNAOpts ),
+     NcbiSymbOpts = [ db(ncbi),
+                 to_value_1(pos_integer), to_value_2(is_a_symbol),
+                 datetime(HsDnDt), source(HsUrl), 
+                 header(row(ncbi,symbol)) 
+                 | Opts
+     ],
+     csv_ids_map( HsStem, 'GeneID', 'Symbol', _Csv3, NcbiSymbF, NcbiSymbOpts ),
+     % delete_file( TmpF ),
+     os_make_path( maps ),
+     @ mv( -f, OutF, maps ),
+     @ mv( -f, DNAF, maps ),
+     @ mv( -f, NcbiSymbF, maps ),
+     working_directory( _, maps ),
+     link_to_bio_sub(ncbi, OutF ),
+     link_to_bio_sub(ncbi, DNAF ),
+     link_to_bio_sub(ncbi, NcbiSymbF ),
+     working_directory( _, Old ).
+
+ncbi_humanise_data( Stem, Dir, Repo, Old, HsStem, Url, DnDt, Opts ) :-
+     file_name_extension( Stem, gz, GzF ),
+     os_path( Repo, GzF, Url ),
+     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)|Opts] ),
+     os_path( Dir, GzF, DnlF ),
+     bio_db_dnt_times( DnlF, DnDt, _DnEn ),
+     working_directory( Old, Dir ),
+     % atomic_list_concat( [Stem,hs], '_', HsStem ),
+     % @ rm( -f, HsStem ),
+     @ rm( -f, Stem ),
+     @ gunzip( -f, -k, GzF ),
+     ncbi_species_grep( Stem, HsStem, Opts ),
+     @ rm( -f, Stem ).
+
+% maps_ncbi_ensp_ensg.
+%
+% This is a later addition.
+% See ens_fa_peptide_gene_rows..pl
+%
+maps_ncbi_ensp_ensg :-
+     % Dir = '/usr/local/users/nicos/work/db/data/ncbi',
+     fixme,
+     ncbi_dnload( Dir ),
+     EnsF= 'Homo_sapiens.GRCh38.pep.all.fa',
+     working_directory( Old, Dir ),
+     ens_fa_peptide_gene_rows( EnsF, EnsRows ),
+     csv_ids_map( _CsvF, ensp, ensg, EnsRows, OutF, [prefix(ncbi),header(row('Ensembl Protein','Ensembl Gene'))] ),
+     link_to_bio_sub(ncbi, OutF ),
+     working_directory( _, Old ).
+
+% fixme: this is not in the loop ? either fix or remove
+maps_ncbi_ncbi_gont( Opts ) :-
+     % Dir = '/usr/local/users/nicos/work/db/data/ncbi',
+     ncbi_dnload( Dir ),
+     ncbi_repo( Repo ),
+     os_path( Repo, 'gene2go.gz', Url ),
+     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)] ),
+     working_directory( Old, Dir ),
+     @ rm( -f, gene2go_hs ),
+     @ rm( -f, gene2go ),
+     @ gunzip( -f, -k, 'gene2go.gz' ),
+     % debuc( by_unix ),
+     ncbi_species_grep( gene2go, _HsStem, Opts ),  % pass sep(tab)
+     % os_grep_mtx(gene2go, '^9606', gene2go_hs, true ),
+     % system( 'grep "^9606" gene2go | cat gene2go_hs' ),
+     working_directory( _, Old ).
+
 pos_integer( Numb, Numb ) :-
      integer( Numb ),
      !,
@@ -266,10 +234,25 @@ pfx_by_de_v( Pfx, Full, UnV ) :-
 pfx_by( Pfx, Full, Full ) :-
      prefix_atom( Pfx, Full ).
 
-ncbi_cname_known( 'HGNC Symbol', symb ).
-ncbi_cname_known( 'Ensembl Gene', ensg ).
-ncbi_cname_known( 'Ensembl Protein', ensp ).
-ncbi_cname_known( 'Entrez ID', ncbi ).
-ncbi_cname_known( 'Uni Gene', unig ).
-ncbi_cname_known( 'RNA Nucleotide', rnuc ).
-ncbi_cname_known( 'DNA Nucleotide', dnuc ).
+is_a_symbol( Symb, Symb ) :-
+     hgnc:hgnc_homs_symb_hgnc( Symb, _ ),
+     !.
+
+/** %unigene is no longer maintained as of Feb.2019
+maps_ncbi_unig_ncbi :-
+     ncbi_dnload( Dir ),
+     ncbi_repo( Repo ),
+     os_path( Repo, 'gene2unigene', Url ),
+     url_file_local_date_mirror( Url, Dir, [debug(true),interface(wget)] ),
+     working_directory( Old, Dir ),
+     bio_db_dnt_times( 'gene2unigene', UgDnDt, _DnEn ),
+     csv_read_file( gene2unigene, [_|Csv], [separator(0'\t),match_arity(false)] ),
+     Hdr = row(ncbi,unig),
+     MOpts = [prefix(ncbi),to_value_1(hs_unig),datetime(UgDnDt),source(Url),header(row('Uni Gene','Entrez ID'))], 
+     csv_ids_map( _, 'unig', 'ncbi', [Hdr|Csv], OutF, MOpts ),
+     os_make_path( maps ),
+     @ mv( -f, OutF, maps ),
+     working_directory( _, maps ),
+     link_to_bio_sub( ncbi, OutF ), 
+     working_directory( _, Old ).
+*/

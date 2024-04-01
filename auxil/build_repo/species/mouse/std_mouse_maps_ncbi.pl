2a853b495bb65bfdb3010b4698153b7186cab807
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-04-01 21:33:57 +0100

    ADDED:   mouse support for ncbi synonyms (via lib now)

--- auxil/build_repo/species/mouse/std_mouse_maps_ncbi.pl
+++ auxil/build_repo/species/mouse/std_mouse_maps_ncbi.pl
@@ -15,13 +15,19 @@
 % local
 :- lib(ncbi_std_maps/1).
 
-std_mouse_maps_ncbi_defaults( [org(mouse)] ).
+std_mouse_maps_ncbi_defaults( Defs ) :-
+          Defs = [
+                    ncbi_gene_info('GENE_INFO/Mammalia/Mus_musculus.gene_info.gz'),
+                    org(mouse)
+                 ].
 
 /** std_mouse_maps_ncbi(+Opts).
 
 Build starndard NCBI maps for mouse. 
 
 Opts
+ * ncbi_gene_info(GnInf='GENE_INFO/Mammalia/Mus_musculus.gene_info.gz')
+   genes synonyms file
  * org(Org=mouse)
    organism
 
@@ -34,5 +40,5 @@ All code has moved to lib(ncbi_std_maps.pl) as it is can be used for other speci
 std_mouse_maps_ncbi( Args ) :-
      Self = std_mouse_maps_ncbi,
      options_append( Self, Args, Opts ),
-     ncbi_mouse_std_maps( Opts ).
+     ncbi_std_maps( Opts ).
 

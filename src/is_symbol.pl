ce5f785f3f2b08920c33cf373eaceebbe596bba1
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-10-19 23:15:27 +0100

    FIXED:   missing is_symbol/2 for chicen and pig

--- src/is_symbol.pl
+++ src/is_symbol.pl
@@ -12,11 +12,16 @@ true.
 
 @author nicos angelopoulos
 @version  0:1 2019/4/7
+@tbd this needs to be more robust... needs also to be updated when new organisms are updated
 
 */
+is_symbol( chicken, Symb ) :-
+     cgnc_galg_cgnc_symb( _, Symb ).
 is_symbol( human, Symb ) :-
     is_symbol( hs, Symb ).
 is_symbol( hs, Symb ) :-
     map_hgnc_hgnc_symb( _, Symb ).
 is_symbol( mouse, Symb ) :-
     map_mgim_mouse_mgim_symb( _, Symb ).
+is_symbol( pig, Symb ) :-
+     ncbi_suss_ncbi_symb( _, Symb ).
